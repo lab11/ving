6e8e62a9661e67e9806e99626389da972499a3b1
genevee <genevee@umich.edu>
2014-11-19 16:12:56 -0500

    changed

--- MPU_6500/MPU_6500.ino
+++ MPU_6500/MPU_6500.ino
@@ -1,17 +1,25 @@
 #include <SoftwareSerial.h>
 #include <SPI.h>
+#include <Time.h>  
+
+#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 //Sensor's memory register addresses:
 const int ledpin = 19;
 const int chipSelectPin = 7;
+const int resetOpenLog = 2;
+SoftwareSerial OpenLog(4, 3); //Soft RX on 4, Soft TX out on 3
 
    int dataReadyL = 0;
    int dataReadyH = 0;
    byte acc_X[2] = {0,0};
-   long acc_full = 0;
-   byte testh = 1;
-   byte testl = 1;
+   uint16_t acc_full = 0;
+   uint8_t testh = 1;
+   uint8_t testl = 1;
    byte test = 0;
+   uint16_t time;
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 //Connect TXO of OpenLog to pin 3, RXI to pin 2
@@ -23,10 +31,9 @@ int ledPin = 19;
 
 
 void setup() {                
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledpin, OUTPUT);
   Serial.begin(9600);
-
-
+ 
   // start the SPI library:
   SPI.begin();
 
@@ -50,7 +57,9 @@ void setup() {
 //  writeRegister(108, 0x1F); //Disable all axis and gryo except ACC_X 0001 1111
   writeRegister(108, 0x00); //Disable all axis and gryo except ACC_X 0001 1111
 
+  Serial.println("SPI initialized");
   // give the sensor time to set up:
+
   delay(100);
 }
 
@@ -62,48 +71,44 @@ void loop() {
    delay(100);
    digitalWrite(ledpin, HIGH);
 
-    // don't do anything until the data ready pin is high:
-    //Read the temperature data
-    //testh = readRegister(59, 1);
+    testh = readRegister(59, 1);
+    testl = readRegister(60, 1);
     //delay(10);
-    //testl = readRegister(60, 1);
-    //delay(10);
-    //acc_full = (testh << 16) | testl;
+    acc_full = ((testh << 8) | testl);
     //test = readRegister(0x75, 0); //Reg 117
-    dataReadyL = readRegister(0x73, 1); //Read from FIFO_CNT_L 
-    dataReadyH = readRegister(0x72, 1); //Read from FIF0_CNT 0x72, latch new value
-        /*Serial.print("ReadyL=");
+    //dataReadyL = readRegister(0x73, 1); //Read from FIFO_CNT_L 
+    //dataReadyH = readRegister(0x72, 1); //Read from FIF0_CNT 0x72, latch new value
+    /*Serial.print("ReadyL=");
     Serial.print(dataReadyH);
-        Serial.print("  ReadyH=");
+    Serial.print("  ReadyH=");
     Serial.println(dataReadyL);*/
-    //Serial.print("Test=");
-    //Serial.println(acc_full);
+   
     
-    if (dataReadyL | dataReadyH) { //If there is data to be read in the FIFO
+    /*if (dataReadyL | dataReadyH) { //If there is data to be read in the FIFO
         //digitalWrite(ledpin, HIGH);
          //acc_X[0] = readRegister(0x74, 1); //Read from 0x74 FIFO_R_W ACC_X_H
          //acc_X[1] = readRegister(0x74, 1); //Read from 0x74 FIFO_R_W ACC_X_L
-         //acc_full = ((acc_X[0] << 15) | acc_X[1]);
+         //acc_full = ((acc_X[0] << 8) | acc_X[1]);
          delay(10);
          //digitalWrite(ledpin, LOW);
-         acc_full = readRegister(0x74, 2); //Read from 0x74 FIFO_R_W ACC_X_H
+
+           acc_full = readRegister(0x74, 2); //Read from 0x74 FIFO_R_W ACC_X_H
     } 
     else{
          digitalWrite(ledpin, HIGH);
          delay(100);
          digitalWrite(ledpin, LOW);
          delay(100);
-    }
+    }*/
     // convert the temperature to celsius and display it:
+   
+   
     /*Serial.print("ACC_X[0]=");
    Serial.print(acc_X[0]);
    Serial.print("   ACC_X[1]=");
    Serial.println(acc_X[1]);*/
-    Serial.print("Acc_X ");
-    Serial.println(acc_full);
-  
     //Serial.print("   Test =");
-    //Serial.println(test);
+    Serial.println((int)(acc_full));
 }
 
 //Read from or write to register from the SCP1000:
@@ -158,4 +163,44 @@ void writeRegister(byte thisRegister, byte thisValue) {
 
   // take the chip select high to de-select:
   digitalWrite(chipSelectPin, HIGH);
+}
+
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+void processSyncMessage() {
+  // if time sync available from serial port, update time and return true
+  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of header & 10 ASCII digits
+    char c = Serial.read() ; 
+    Serial.print(c);  
+    if( c == TIME_HEADER ) {       
+      time_t pctime = 0;
+      for(int i=0; i < TIME_MSG_LEN -1; i++){   
+        c = Serial.read();          
+        if( c >= '0' && c <= '9'){   
+          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
+        }
+      }   
+      setTime(pctime);   // Sync Arduino clock to the time received on the serial port
+    }  
+  }
 }
