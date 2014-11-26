7353cfeaff4398b8c611154df1099fa2451fe8dd
Josh Adkins <adkinsjd@umich.edu>
2014-11-25 19:36:05 -0500

    Fixed directory organization

--- arduino-prototyping/FFT_tests/spectrum2/spectrum2.ino
+++ arduino-prototyping/FFT_tests/spectrum2/spectrum2.ino
@@ -4,25 +4,31 @@
 // This code is part of the guide at http://learn.adafruit.com/fft-fun-with-fourier-transforms/
 
 #define ARM_MATH_CM4
+#define MPU_ADDRESS 0x68
+
 #include <arm_math.h>
 #include <Adafruit_NeoPixel.h>
-
+#include <Wire.h>
+void writeRegister(uint8_t,uint8_t);
+uint8_t readRegister(uint8_t);
 
 ////////////////////////////////////////////////////////////////////////////////
 // CONIFIGURATION 
 // These values can be changed to alter the behavior of the spectrum display.
 ////////////////////////////////////////////////////////////////////////////////
 
-int SAMPLE_RATE_HZ = 9000;             // Sample rate of the audio in hertz.
+int SAMPLE_RATE_HZ = 1000;             // Sample rate of the audio in hertz.
 float SPECTRUM_MIN_DB = 30.0;          // Audio intensity (in decibels) that maps to low LED brightness.
 float SPECTRUM_MAX_DB = 60.0;          // Audio intensity (in decibels) that maps to high LED brightness.
 int LEDS_ENABLED = 1;                  // Control if the LED's should display the spectrum or not.  1 is true, 0 is false.
                                        // Useful for turning the LED display on and off with commands from the serial port.
-const int FFT_SIZE = 256;              // Size of the FFT.  Realistically can only be at most 256 
+const int FFT_SIZE = 64;              //256 Size of the FFT.  Realistically can only be at most 256 
                                        // without running out of memory for buffers and other state.
-const int AUDIO_INPUT_PIN = 14;        // Input ADC pin for audio data.
-const int ANALOG_READ_RESOLUTION = 10; // Bits of resolution for the ADC.
-const int ANALOG_READ_AVERAGING = 16;  // Number of samples to average with each ADC reading.
+                                       
+const int AUDIO_INPUT_PIN = 14;        // Input pin for audio data.
+const int READ_RES = 16;               // Bits of resolution for the ADC.
+
+const int ANALOG_READ_AVERAGING = 4;  // Number of samples to average with each ADC reading.
 const int POWER_LED_PIN = 13;          // Output pin for power LED (pin 13 to use Teensy 3.0's onboard LED).
 const int NEO_PIXEL_PIN = 3;           // Output pin for neo pixels.
 const int NEO_PIXEL_COUNT = 4;         // Number of neo pixels.  You should be able to increase this without
@@ -35,15 +41,31 @@ const int MAX_CHARS = 65;              // Max size of the input command buffer
 // These shouldn't be modified unless you know what you're doing.
 ////////////////////////////////////////////////////////////////////////////////
 
-IntervalTimer samplingTimer;
-float samples[FFT_SIZE*2];
-float magnitudes[FFT_SIZE];
-int sampleCounter = 0;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_PIXEL_COUNT, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 char commandBuffer[MAX_CHARS];
 float frequencyWindow[NEO_PIXEL_COUNT+1];
 float hues[NEO_PIXEL_COUNT];
 
+IntervalTimer samplingTimer;
+float samples[FFT_SIZE*2];
+float magnitudes[FFT_SIZE];
+float mag_f[FFT_SIZE];
+int sampleCounter = 0;
+
+q31_t samples_fix[FFT_SIZE*2];
+//q31_t samples_h_fix[FFT_SIZE*2];
+q31_t magnitudes_fix[FFT_SIZE];
+q31_t max_finder[FFT_SIZE];
+
+uint8_t high = 0;
+uint8_t low = 0;
+q31_t val = 0;
+int holding = 0;
+q31_t max_val = 0;
+uint32_t max_index = 0;
+float32_t test = 0;
+int dom_freq_h = 0;
+int dom_freq_l = 0;
 
 ////////////////////////////////////////////////////////////////////////////////
 // MAIN SKETCH FUNCTIONS
@@ -52,10 +74,11 @@ float hues[NEO_PIXEL_COUNT];
 void setup() {
   // Set up serial port.
   Serial.begin(38400);
+  Wire.begin();
   
   // Set up ADC and audio input.
   pinMode(AUDIO_INPUT_PIN, INPUT);
-  analogReadResolution(ANALOG_READ_RESOLUTION);
+  analogReadResolution(READ_RES);
   analogReadAveraging(ANALOG_READ_AVERAGING);
   
   // Turn on the power indicator LED.
@@ -77,14 +100,85 @@ void setup() {
 }
 
 void loop() {
+  Serial.print("here");
+  high = readRegister(63);
+  low = readRegister(64);
+  Serial.print("here");
   // Calculate FFT if a full sample is available.
   if (samplingIsDone()) {
     // Run FFT on sample data.
-    arm_cfft_radix4_instance_f32 fft_inst;
+    //Serial.print("Sampling Completed");
+    /*arm_cfft_radix4_instance_f32 fft_inst;
     arm_cfft_radix4_init_f32(&fft_inst, FFT_SIZE, 0, 1);
     arm_cfft_radix4_f32(&fft_inst, samples);
     // Calculate magnitude of complex numbers output by the FFT.
-    arm_cmplx_mag_f32(samples, magnitudes, FFT_SIZE);
+    arm_cmplx_mag_f32(samples, magnitudes, FFT_SIZE);*/
+    
+    //////////////////////////////////////////////////////////////////////////////////
+    
+    //unsigned long beg = micros();
+    arm_cfft_radix4_instance_q31 fft_inst_fix;
+    int status = arm_cfft_radix4_init_q31(&fft_inst_fix, FFT_SIZE, 0, 1); //Selects forward transform and bit reversal of output
+    if (status == ARM_MATH_ARGUMENT_ERROR){
+        Serial.print(status);
+        Serial.println("FFT Init ERROR!");
+    }
+    
+    //Calculate the FFT of samples and upscale output by 6 bits to retain q31_t data format
+    
+    //Serial.println(samples_fix[0], DEC);
+    arm_cfft_radix4_q31(&fft_inst_fix, samples_fix);
+    /*Serial.print("After: ");
+    Serial.println(samples_fix[0], DEC);
+    
+    //Serial.println(samples_fix[0]);
+    Serial.print("Before: ");
+    Serial.println(samples_fix[0], HEX);*/
+    
+    
+
+    
+    // Calculate magnitude of complex numbers outq31put by the FFT.
+    arm_cmplx_mag_q31(samples_fix, magnitudes_fix, FFT_SIZE);
+    
+    //Serial.print("Sample: ");
+    //Serial.println((magnitudes_fix[0] >> (30-READ_RES)), DEC);
+   
+    for (int i = 0; i < FFT_SIZE/2; ++i) {
+      max_finder[i] = magnitudes_fix[i];
+    }
+    max_finder[0] = 0;
+    arm_max_q31(max_finder, FFT_SIZE, &max_val, &max_index);
+    
+    dom_freq_l = (SAMPLE_RATE_HZ/FFT_SIZE)*max_index;
+    dom_freq_h = dom_freq_l + (SAMPLE_RATE_HZ/FFT_SIZE);
+    
+    /*Serial.print("Dominent Freq: ");
+    Serial.print(dom_freq_l);
+    Serial.print(" - ");
+    Serial.print(dom_freq_h);
+    Serial.print("  Val: ");
+    Serial.println(((max_val >> (30-READ_RES))/32767.0)*1.8);*/
+    
+    /*Serial.print("Max val: ");
+    Serial.print(max_val);
+    Serial.print("   Max index:  ");
+    Serial.println(max_index);  */
+    
+    //arm_float_to_q31(
+    //arm_q31_to_float(magnitudes_fix, mag_f, FFT_SIZE);
+    //unsigned long sto  = micros();
+    //unsigned long delta = sto - beg;
+    //Serial.println(delta);    
+    
+    for (int i = 0; i < FFT_SIZE; ++i) {
+      //magnitudes[i] = (magnitudes_fix[i] >> 15);
+      magnitudes[i] = (magnitudes_fix[i] >> 12);
+      //Serial.println(magnitudes[i]);
+     // magnitudes[i] = (float)(magnitudes_fix[i] >> 10);
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////
   
     if (LEDS_ENABLED == 1)
     {
@@ -93,13 +187,18 @@ void loop() {
   
     // Restart audio sampling.
     samplingBegin();
-  }
     
+    //high = readRegister(63);
+    //low = readRegister(64);
+    //val = (high<<8)+low;
+    //val = (val << 30-READ_RES);
+    //Serial.println(val, BIN);
+   
+  }
+
   // Parse any pending commands.
   parserLoop();
 }
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // UTILITY FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////
@@ -217,10 +316,31 @@ void spectrumLoop() {
 
 void samplingCallback() {
   // Read from the ADC and store the sample data
-  samples[sampleCounter] = (float32_t)analogRead(AUDIO_INPUT_PIN);
+   high = readRegister(63);
+   low = readRegister(64);
+   val = (q31_t)((high<<8)+low);
+   Serial.println(val);
+   //val = (val << 30-READ_RES);
+   samples_fix[sampleCounter] = val;
+   
+   //Input samples and convert to q31_t data bit (signbit.31resolution bits)
+   //test = ((float32_t)((analogRead(AUDIO_INPUT_PIN)) & 0xFFFF)/(0xFFFF/2)-1);
+   //Serial.print("Before cast:");
+   //Serial.println(holding);
+   holding = analogRead(AUDIO_INPUT_PIN);
+   samples[sampleCounter] = holding;
+   
+   //holding = ((float32_t)(holding/65535));
+   //arm_float_to_q31()
+   //samples_fix[sampleCounter] = (q31_t)(holding << (31-READ_RES));
+   //Serial.print("After cast:");
+   //Serial.println(samples_fix[sampleCounter], BIN);
+  
   // Complex FFT functions require a coefficient for the imaginary part of the input.
-  // Since we only have real data, set this coefficient to zero.
+  // Since we only havGETe real data, set this coefficient to zero.
   samples[sampleCounter+1] = 0.0;
+  samples_fix[sampleCounter+1] = (q31_t)0;  
+ 
   // Update sample buffer position and stop after the buffer is filled
   sampleCounter += 2;
   if (sampleCounter >= FFT_SIZE*2) {
@@ -285,17 +405,19 @@ void parserLoop() {
 void parseCommand(char* command) {
   if (strcmp(command, "GET MAGNITUDES") == 0) {
     for (int i = 0; i < FFT_SIZE; ++i) {
-      Serial.println(magnitudes[i]);
+      Serial.println(mag_f[i]);
     }
   }
   else if (strcmp(command, "GET SAMPLES") == 0) {
-    for (int i = 0; i < FFT_SIZE*2; i+=2) {
+    for (int i = 0; i < FFT_SIZE*2; i+=1) {
       Serial.println(samples[i]);
     }
   }
   else if (strcmp(command, "GET FFT_SIZE") == 0) {
     Serial.println(FFT_SIZE);
   }
+  
+
   GET_AND_SET(SAMPLE_RATE_HZ)
   GET_AND_SET(LEDS_ENABLED)
   GET_AND_SET(SPECTRUM_MIN_DB)
@@ -314,3 +436,30 @@ void parseCommand(char* command) {
     pixels.show();
   }
 }
+
+void writeRegister(uint8_t reg, uint8_t val)
+{
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(reg);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+uint8_t readRegister(uint8_t reg)
+{
+  Wire.beginTransmission(MPU_ADDRESS);
+  Wire.write(reg);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(MPU_ADDRESS,1);
+  
+  uint8_t ret = 0;
+  while(Wire.available())
+  {
+    ret = Wire.read();
+  }
+    
+  return ret;
+}
+  
+
